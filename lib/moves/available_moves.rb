f46c4cf66a316db5909c59ddd6e3021fddad58fa
Reyad <20869625+AReyad@users.noreply.github.com>
2026-03-05 08:25:21 +0200

    feat(move): create availablemoves && moveconverter

---
+++ lib/moves/available_moves.rb
@@ -0,0 +1,55 @@
+module AvailableMoves
+  LOWEST_BOARD_INDEX = 0
+  HIGHEST_BOARD_INDEX = 7
+
+  def add_two_moves(first_move, second_move)
+    [first_move[0] + second_move[0], first_move[1] + second_move[1]]
+  end
+
+  def all_moves(position, board)
+    return consecutive_moves(position, board) if moves_consecutively?
+
+    regular_moves(position) if moves_regularly?
+  end
+
+  def consecutive_moves(position, board)
+    result = []
+    consecutive_move_set.each do |move|
+      current_step = position
+      loop do
+        current_step = add_two_moves(move, current_step)
+        result << current_step
+
+        break if board.occupied_square?(current_step) || !move_within_range?(current_step)
+      end
+    end
+    result
+  end
+
+  def regular_moves(position)
+    result = []
+    regular_move_set.each do |move|
+      current_step = position
+      move.each do |step|
+        current_step = add_two_moves(step, current_step)
+      end
+      result << current_step
+    end
+    result
+  end
+
+  def available_moves(position, board)
+    all_moves(position, board)
+      .select { |move| move_within_range?(move) && !team_occupied_square?(move, board) }
+  end
+
+  def team_occupied_square?(position, board)
+    other_piece = board.at(position)
+    color == other_piece&.color
+  end
+
+  def move_within_range?(move)
+    move[0].between?(LOWEST_BOARD_INDEX, HIGHEST_BOARD_INDEX) &&
+      move[1].between?(LOWEST_BOARD_INDEX, HIGHEST_BOARD_INDEX)
+  end
+end
---
+++ lib/moves/move_converter.rb
@@ -0,0 +1,21 @@
+module MoveConverter
+  FILE_TO_INDEX = { 'a' => 0, 'b' => 1, 'c' => 2, 'd' => 3, 'e' => 4, 'f' => 5, 'g' => 6, 'h' => 7 }.freeze
+  def convert_move(move)
+    return convert_algebraic_move(move) if move.is_a?(String)
+
+    convert_coord_move(move)
+  end
+
+  def convert_algebraic_move(string)
+    rank = 8 - string[1].to_i
+    file = FILE_TO_INDEX[string[0]].to_i
+    [rank, file]
+  end
+
+  def convert_coord_move(coord)
+    rank = (8 - coord[0]).to_s
+    file = coord[1]
+    algebraic_letter = FILE_TO_INDEX.key(file)
+    algebraic_letter + rank
+  end
+end
