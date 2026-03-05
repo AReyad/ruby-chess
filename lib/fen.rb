92726c548a6945d11bd8179654dcc627df041db5
Reyad <20869625+AReyad@users.noreply.github.com>
2026-03-05 08:24:09 +0200

    feat(fen): create fen module

---
+++ lib/fen.rb
@@ -0,0 +1,61 @@
+module Fen
+  INITIAL_PIECE_PLACEMENT = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
+
+  CHESS_PIECES_NOTATION = { 'n' => Knight.new('black'), 'r' => Rook.new('black'), 'b' => Bishop.new('black'),
+                            'q' => Queen.new('black'), 'k' => King.new('black'), 'p' => Pawn.new('black'),
+                            'N' => Knight.new('white'), 'R' => Rook.new('white'), 'B' => Bishop.new('white'),
+                            'Q' => Queen.new('white'), 'K' => King.new('white'), 'P' => Pawn.new('white') }.freeze
+
+  module_function
+
+  def board_fen_array(string)
+    fen_array = substitute_fen_digits(string).split('/')
+    fen_array.map do |string|
+      string.split('')
+    end
+  end
+
+  def generate_board(string = INITIAL_PIECE_PLACEMENT)
+    fen_array = board_fen_array(string)
+    fen_array.map do |array|
+      array.map do |ele|
+        ele = CHESS_PIECES_NOTATION[ele]
+        ele if CHESS_PIECES_NOTATION[ele].nil?
+      end
+    end
+  end
+
+  def parse_fen(string)
+    fen_sequences = fen_string_to_array(string)
+    board_from_fen(fen_sequences)
+  end
+
+  def substitute_fen_digits(string)
+    string.gsub(/\d/) { |match| ' ' * match.to_i }
+  end
+
+  def sum_fen_digits(string)
+    string.gsub(/\d+/) do |match|
+      sum = 0
+      match.each_char do |digit|
+        sum += digit.to_i
+      end
+      sum.to_s
+    end
+  end
+
+  def board_to_fen(board)
+    string = ''
+    board.each do |row|
+      row.each do |ele|
+        key = CHESS_PIECES_NOTATION.key(ele)
+        key = '1' if ele.nil?
+        string += key
+      end
+      string += '/'
+    end
+    sum_fen_digits(string)
+  end
+
+  # module_function :board_from_fen, :board_fen_array, :sum_fen_digits, :substitute_fen_digits
+end
