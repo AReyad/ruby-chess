162d627e2d2add3bfc930de738edf45b857840f0
Reyad <20869625+AReyad@users.noreply.github.com>
2026-03-07 05:27:52 +0200

    feat(colorize): add highlight method

--- lib/colorize.rb
+++ lib/colorize.rb
@@ -8,4 +8,8 @@ module Colorize
   def foreground(color, content)
     "\e[38;2;#{color}m #{content} \e[0m"
   end
+
+  def highlight(color, first_value, second_value)
+    "\e[38;2;#{color}m#{first_value}#{second_value}\e[0m"
+  end
 end
