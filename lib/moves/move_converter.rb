4f0826c2a8f169fe41fb1be3b718e0ae4121dc87
Reyad <20869625+AReyad@users.noreply.github.com>
2026-03-10 20:26:41 +0200

    feat(MoveConverter): add all methods as module functions

--- lib/moves/move_converter.rb
+++ lib/moves/move_converter.rb
@@ -1,9 +1,18 @@
 module MoveConverter
   FILE_TO_INDEX = { 'a' => 0, 'b' => 1, 'c' => 2, 'd' => 3, 'e' => 4, 'f' => 5, 'g' => 6, 'h' => 7 }.freeze
+
+  module_function
+
   def convert_move(move)
     return convert_algebraic_move(move) if move.is_a?(String)
 
     convert_coord_move(move)
+  end
+
+  def convert_moves(moves)
+    moves.map do |move|
+      convert_move(move)
+    end.join(', ')
   end
 
   def convert_algebraic_move(string)
@@ -17,11 +26,5 @@ module MoveConverter
     file = coord[1]
     algebraic_letter = FILE_TO_INDEX.key(file)
     algebraic_letter + rank
-  end
-
-  def convert_moves(moves)
-    moves.map do |move|
-      convert_move(move)
-    end.join(', ')
   end
 end
