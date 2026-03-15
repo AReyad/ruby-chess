2124d1c265fe40e171988533c8f2e3a170989750
Reyad <20869625+AReyad@users.noreply.github.com>
2026-03-15 14:37:13 +0200

    refactor: change safe_king_moves to safe_moves

--- lib/chess/pieces/king.rb
+++ lib/chess/pieces/king.rb
@@ -3,7 +3,7 @@ module Chess
     DIRECTIONS = [[:north_east], [:north], [:north_west], [:west], [:south_west],
                   [:south], [:south_east], [:east]].freeze
 
-    def safe_king_moves(position, board)
+    def safe_moves(position, board)
       available_moves(position, board)
         .reject { |move| board.king_in_check?(color, move) }
     end
