8bfbd55be27a00ccd925037b6d4c19b16bd847ca
Reyad <20869625+AReyad@users.noreply.github.com>
2026-03-15 14:28:56 +0200

    feat: add safe moves for king

--- lib/chess/pieces/king.rb
+++ lib/chess/pieces/king.rb
@@ -2,5 +2,10 @@ module Chess
   class King < Piece
     DIRECTIONS = [[:north_east], [:north], [:north_west], [:west], [:south_west],
                   [:south], [:south_east], [:east]].freeze
+
+    def safe_king_moves(position, board)
+      available_moves(position, board)
+        .reject { |move| board.king_in_check?(color, move) }
+    end
   end
 end
