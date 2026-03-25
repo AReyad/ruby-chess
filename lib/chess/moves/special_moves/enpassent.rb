dc2c6ce57d7c842d72582a91a4ca2c9fe27db52d
Reyad <20869625+AReyad@users.noreply.github.com>
2026-03-25 19:58:58 +0200

    feat: create #enpassent_position

--- lib/chess/moves/special_moves/enpassent.rb
+++ lib/chess/moves/special_moves/enpassent.rb
@@ -1,4 +1,22 @@
 module Chess
   module EnPassent
+    def  enpassent_position(position, color)
+      return white_enpassent_position(position) if color == 'white'
+
+      black_enpassent_position(position)
+    end
+
+    def white_enpassent_position(position)
+      [position[0] + 1, position[1]]
+    end
+
+    def black_enpassent_position(position)
+      [position[0] - 1, position[1]]
+    end
+
+    def enpassent_to_capture(position, destination, board)
+      piece = board.at(position)
+      enpassent_position(destination, piece.color)
+    end
   end
 end
