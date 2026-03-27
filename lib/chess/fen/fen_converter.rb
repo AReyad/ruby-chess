b4a054326bf5ba412b9de19068f9cb85bc83f45a
Reyad <20869625+AReyad@users.noreply.github.com>
2026-03-28 01:43:44 +0200

    fix: board_to_fen was not working correctly

--- lib/chess/fen/fen_converter.rb
+++ lib/chess/fen/fen_converter.rb
@@ -3,12 +3,12 @@ module Chess
   module FenConverter
     INITIAL_PIECE_PLACEMENT = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'.freeze
 
-    PIECES_NOTATION = { 'n' => { name: Knight, color: 'black' }, 'r' => { name: Rook, color: 'black' },
-                        'b' => { name: Bishop, color: 'black' }, 'q' => { name: Queen, color: 'black' },
-                        'k' => { name: King, color: 'black' },   'p' => { name: Pawn, color: 'black' },
-                        'N' => { name: Knight, color: 'white' }, 'R' => { name: Rook, color: 'white' },
-                        'B' => { name: Bishop, color: 'white' }, 'Q' => { name: Queen, color: 'white' },
-                        'K' => { name: King, color: 'white' },   'P' => { name: Pawn, color: 'white' } }.freeze
+    PIECES_NOTATION = { 'n' => { name: Chess::Knight, color: 'black' }, 'r' => { name: Chess::Rook, color: 'black' },
+                        'b' => { name: Chess::Bishop, color: 'black' }, 'q' => { name: Chess::Queen, color: 'black' },
+                        'k' => { name: Chess::King, color: 'black' },   'p' => { name: Chess::Pawn, color: 'black' },
+                        'N' => { name: Chess::Knight, color: 'white' }, 'R' => { name: Chess::Rook, color: 'white' },
+                        'B' => { name: Chess::Bishop, color: 'white' }, 'Q' => { name: Chess::Queen, color: 'white' },
+                        'K' => { name: Chess::King, color: 'white' },   'P' => { name: Chess::Pawn, color: 'white' } }.freeze
 
     def board_fen_array(string)
       fen_array = substitute_fen_digits(string).split('/')
@@ -48,7 +48,7 @@ module Chess
       string = ''
       board.each do |row|
         row.each do |ele|
-          key = PIECES_NOTATION.key(ele)
+          key = PIECES_NOTATION.key({ name: ele.class, color: ele&.color })
           key = '1' if ele.nil?
           string += key
         end
