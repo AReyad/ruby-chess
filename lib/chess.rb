d324e9c0f7617cdc4110d6bd0625ef66e13f32d4
Reyad <20869625+AReyad@users.noreply.github.com>
2026-03-30 00:41:34 +0200

    feat: add load functionality

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,15 +1,82 @@
-require_relative './palette'
-require_relative './pieces/piece'
-require_relative './pieces/bishop'
-require_relative './pieces/king'
-require_relative './pieces/pawn'
-require_relative './pieces/knight'
-require_relative './pieces/queen'
-require_relative './pieces/rook'
-require_relative './board/chess_board'
-require_relative './create_piece'
-require_relative './fen'
-require_relative './colorize'
+require_relative './chess/palette'
+require_relative './chess/colorize'
+require_relative './chess/moves/special_moves/enpassent'
+require_relative './chess/moves/special_moves/castling'
+require_relative './chess/moves/special_moves/promotion'
+require_relative './chess/moves/moves_handler'
+require_relative './chess/pieces/piece'
+require_relative './chess/pieces/king'
+require_relative './chess/pieces/bishop'
+require_relative './chess/pieces/pawn'
+require_relative './chess/pieces/knight'
+require_relative './chess/pieces/queen'
+require_relative './chess/pieces/rook'
+require_relative './chess/fen/fen'
+require_relative './chess/board/chess_board'
+require_relative './chess/create_piece'
+require_relative './chess/player'
+require_relative './chess/cli'
+require_relative './chess/game/game'
+require_relative './chess/game/game_state'
 
 module Chess
+  module_function
+
+  def save_path
+    project_dir = File.expand_path('..', __dir__)
+    File.join(project_dir, 'save.json')
+  end
+
+  def deserialize
+    JSON.parse(File.read(save_path))
+  end
+
+  def create_players
+    players = []
+    current_color = 'white'
+    2.times do
+      name = current_color.capitalize
+      players << Player.new(name, current_color)
+      current_color = 'black'
+    end
+    players
+  end
+
+  def load_game
+    CLI.load_confirmation
+    user_input = gets.chomp.downcase
+    return unless user_input == 'y'
+
+    saves = deserialize
+    select_load(saves)
+  end
+
+  def select_load(saves)
+    keys = saves.keys
+    return saves[keys[0]] if keys.length == 1
+
+    display_saves(keys)
+    loop do
+      selected_save = gets.chomp.to_i
+      next CLI.invalid_load unless saves[keys[selected_save]]
+
+      return saves[keys[selected_save]]
+    end
+  end
+
+  def display_saves(saves)
+    saves.each_with_index { |key, index| puts "#{index}) #{key}" }
+    print '=> Type the number of the save you want to load: '
+  end
+
+  def run
+    loaded = load_game if File.exist?(Chess.save_path)
+    return Game.new.play unless loaded
+
+    players = create_players.reverse if loaded['turn'] == 'b'
+    players = create_players if loaded['turn'] == 'w'
+    Game.new(ChessBoard.new(loaded['placement'], loaded), players).play
+  end
 end
+
+Chess.run
