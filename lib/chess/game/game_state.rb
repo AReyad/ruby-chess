9780555ac92541bc28e1f2d5da1fee1f9c0d808c
Reyad <20869625+AReyad@users.noreply.github.com>
2026-04-02 02:53:05 +0200

    feat: add more draw cases

--- lib/chess/game/game_state.rb
+++ lib/chess/game/game_state.rb
@@ -5,7 +5,7 @@ module Chess
     end
 
     def draw?
-      stalemate?(current_player.color, board)
+      stalemate?(current_player.color, board) || board.threefold? || agree_on_draw?
     end
 
     def game_over?
@@ -17,7 +17,15 @@ module Chess
     end
 
     def stalemate?(color, board)
-      !board.king_can_escape?(color)
+      !board.king_can_escape?(color) && !board.king_in_check?(color)
+    end
+
+    def agree_on_draw?
+      return unless board.fifty_moves?
+
+      puts '=> Do you want to end the game with a draw? Y\N?'
+      print '=> '
+      %w[yes y ye yea yeah draw].include? gets.chomp
     end
   end
 end
